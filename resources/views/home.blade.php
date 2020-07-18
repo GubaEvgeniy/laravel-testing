8cd0e4a1aff76646c4dc4aed1ac506811ddb67bb
gubaevgeniy <evgeniyguba@gmail.com>
2020-07-18 17:40:12 +0300

    add homepage and user auth - use laravel:ui

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Get your benefit') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    @auth
+                        <p>Hello {{ Auth::user()->name }}</p>
+                    @endauth
+
+
+                    <button type="submit" class="btn btn-primary">
+                        {{ __('Get benefit') }}
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
